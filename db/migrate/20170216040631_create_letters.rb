92c8baeb8592daaa39bd758876866af44c2f8f0e
Polly Sileo <pollysileo@gmail.com>
2017-02-15 23:24:50 -0500

    Letters Table

--- db/migrate/20170216040631_create_letters.rb
+++ db/migrate/20170216040631_create_letters.rb
@@ -1,7 +1,8 @@
 class CreateLetters < ActiveRecord::Migration[5.0]
   def change
     create_table :letters do |t|
-
+      t.string :letter
+      t.integer :count, default: 0
       t.timestamps
     end
   end
