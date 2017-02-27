a31fb03b80f6efac16f10f70cd4b6ae3f1930a9d
Polly Sileo <pollysileo@gmail.com>
2017-02-26 20:49:14 -0500

    Calculate percentages of each letter

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -6,12 +6,13 @@ class LettersController < ApplicationController
   def index
     @letter = Letter.new
     @letters = Letter.all
+    @total_letter_count = Letter.all.map(&:count).sum
   end
 
   def create
     words = params.fetch("letter")["words"]
     letters_array = words.split("")
-    ingorable_letters = [" ", "'", '"', ",", "!", "."]
+    ingorable_letters = [" ", "'", '"', ",", "!", ".", "'"]
     letters_array.each do |letter|
       next if ingorable_letters.include? letter
       object = Letter.where(letter: letter.upcase).first_or_create!
